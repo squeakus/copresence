5935d153718139abe07ec309d4c0581118129d6e
jonathan <jonathanbyrn@gmail.com>
2015-04-17 15:38:05 +0100

    two way communication

---
+++ leapserver.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('msg:' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+// Send current time to all connected clients
+function sendTime() {
+    io.sockets.emit('time', { time: new Date().toJSON() });
+}
+
+// Send current time every 10 secs
+setInterval(sendTime, 10000);
+
+
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
