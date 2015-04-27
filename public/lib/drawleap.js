545713d70fe9561e3d2004e8315e39b5a0a962c2
jonathan <jonathanbyrn@gmail.com>
2015-04-27 18:31:17 +0100

    now functionised and predicting

--- public/lib/drawleap.js
+++ public/lib/drawleap.js
@@ -1,68 +1,114 @@
 (function(){
     uid = 0; // Unique ID for every user
+    // settings for drawing the circle
+    radius = 5;
+    trail = true;
+    queue = [];
+
+    //This function is to scroll on the chat window
     window.setInterval(function() {
 	var elem = document.getElementById('chat');
 	elem.scrollTop = elem.scrollHeight;
     }, 1000);
     
-    // get the canvas, 2d context, paragraph for data and set the radius
-    canvas = document.getElementsByTagName('canvas')[0],
-    ctx = canvas.getContext('2d'),
-    info = document.getElementById('data'),
+    // get the canvas, 2d context, paragraph for data, set the radius
+    canvas = document.getElementsByTagName('canvas')[0];
+    ctx = canvas.getContext('2d');
+    ctx.fillStyle = "rgba(0,0,0,0.9)";
+    ctx.lineWidth = 10;
+
+    info = document.getElementById('data');
+
     // set the canvas to cover the screen
     canvas.width = document.getElementById('section').clientWidth;
     canvas.height = document.getElementById('section').clientHeight;
     
-    // move the context co-ordinates to the bottom middle of the screen
+    // move context co-ordinates to the bottom middle of the screen
     ctx.translate(canvas.width/2, canvas.height);
     
-    // settings for drawing the circle
-    radius = 10;
-    ctx.fillStyle = "rgba(0,0,0,0.9)";
-    ctx.lineWidth = 5;
+    function setupsockets() {    
+	// set up the socket and listen for the following events
+	socket = io();
+	
+	socket.on('welcome', function(data) {
+            $('#username').text("Welcome user "+ data);
+	    uid = data;
+	});
+	
+	socket.on('message', function(data) {
+            $('#messages').append('<li>' + data + '</li>');
+	});
+	
+	socket.on('update', function(pos) {
+	    x = Math.round(pos[0]);
+	    y = Math.round(pos[1]);
+	    z = Math.round(pos[2]);
+	    queue.push([x,y]);
+	    
+	    if(queue.length > 5){
+		queue.shift();
+	    }
+	    
+	    prediction = predict(queue);
+            $('#location').text("x" + x + " y " + y + " z " + z);
+	    drawcircle(pos, "rgba(255,255,255,0.9)")
+	    drawcircle(prediction, "rgba(255,0,0,0.9)")
+	    
+	});
     
-    // set up the socket and listen for the following events
-    socket = io();
-    
-    socket.on('welcome', function(data) {
-        $('#username').text("Welcome user "+ data);
-	uid = data;
-    });
+	// transmit message on form submission
+	$('form').submit(function(){
+	    socket.emit('message', [uid, $('#m').val()]);
+	    $('#m').val('');
+	    return false;
+	});
+    }
 
-    socket.on('message', function(data) {
-        $('#messages').append('<li>' + data + '</li>');
-    });
+    function predict(queue) {
+	delta = [0,0,0];
+	for (i = 0; i < queue.length - 1; i++) {
+	    for (j = 0; j < queue[i].length; j++)
+		delta[j] += queue[i][j] - queue[i+1][j];
+	}
 
-    socket.on('update', function(pos) {
-        $('#location').text(pos);
-	drawcircle(pos, "rgba(0,255,0,0.9)")
-    });
-    
-    // transmit message on form submission
-    $('form').submit(function(){
-    socket.emit('message', [uid, $('#m').val()]);
-    $('#m').val('');
-    return false;
-    });
-
+	newpos = [0,0,0];
+	lastpos = queue[0];
+	for (i = 0; i < lastpos.length; i ++){
+	    newpos[i] = lastpos[i] - (delta[i] * 2);
+	}
+	return newpos;
+    }
 
     function drawcircle(pos, color) {
 	ctx.strokeStyle = color;
-	// cover the canvas with a 10% opaque layer for fade out effect.
-	ctx.fillStyle = "rgba(255,255,255,0.1)";
-	ctx.fillRect(-canvas.width/2,-canvas.height,canvas.width,canvas.height);
-	// set the fill to black for the points
-	ctx.fillStyle = "rgba(0,0,0,0.9)";
+
+	// cover the canvas with a 10% opaque layer for fade out effect
+	if(trail){
+	    ctx.fillStyle = "rgba(200,200,200,0.1)";
+	}
+	else{
+	    ctx.fillStyle = "rgba(200,200,200,1)";
+	}
+	ctx.fillRect(-canvas.width/2,
+		     -canvas.height,
+		     canvas.width,
+		     canvas.height);
+
 
 	// draw the circle where the pointable is
-	ctx.beginPath();
-	x = pos[0];
-	y = pos[1];
+	x = pos[0] * 2;
+	y = pos[1] * 2;
+	circx = x-radius/2;
+	circy = -(y-radius/2);
 
-	newx = x-radius/2;
-	newy = -(y-radius/2);
-	ctx.arc(newx ,newy ,radius,0,2*Math.PI);
+	// set the fill to white for the points
+	ctx.beginPath();
+	ctx.fillStyle = "rgba(255,255,255,0.9)";
+
+	//centre of the circle, not the bottom right coord
+	ctx.arc(circx ,circy ,radius,0,2*Math.PI);
 	ctx.fill();
+	ctx.lineWidth = 4;
 	ctx.stroke();
     };
 
@@ -76,25 +122,35 @@
 	    // get the pointable and its position
 	    pos = frame.hands[i].palmPosition;
 	    socket.emit('newposition', pos);
-	    console.log(pos);
 	    // add the position data to our data array
 	    data.push(pos);
 	    drawcircle(pos, "rgba(0,0,255,0.9)");
 	}
     };
-
-    document.addEventListener('keydown', function(event) {
-    if (event.keyCode == 37) {
-        radius += 1;
-
+    
+    function keylistener(){
+	document.addEventListener('keydown', function(event) {
+	    // up arrow increase circle size
+	    if (event.keyCode == 38) {
+		if (radius < 20){
+		    radius += 1;
+		}
+	    }
+	    // down arrow decrease circle size
+	    else if (event.keyCode == 40) {
+		if (radius > 5){
+		    radius -= 1;
+		}
+	    }
+	    // t turns off the trail
+	    else if (event.keyCode == 84) {
+		trail = !(trail)
+	    }
+	}, true);
     }
-    else if (event.keyCode == 39) {
-	if (radius > 5){
-            radius -= 1;
-	}
-    }
-}, true);
-
-  // run the animation loop with the draw command
-  Leap.loop(draw);
+    
+    setupsockets();
+    keylistener();
+    // run the animation loop with the draw command
+    Leap.loop(draw);
 })();
