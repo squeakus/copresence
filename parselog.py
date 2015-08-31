641b41dc2d5f0bcae3300ecbba594594d10d551c
jonathan <jonathanbyrn@gmail.com>
2015-08-31 13:47:46 +0100

    now logging differences in prediction

--- parselog.py
+++ parselog.py
@@ -2,12 +2,12 @@ import pylab as P
 import numpy as np
 import sys
 
-SHOWGRAPH = True
+SHOWGRAPH = True #Show graphs after saving to a file
 
 def main():
     # User must specify which log
     if len(sys.argv) != 2:
-        print "uasge: python parselog.py <logname>"
+        print "usage: python parselog.py <logname>"
         exit()
     logname = sys.argv[1].rstrip('.txt')
     logfile = open(sys.argv[1],'r')
@@ -15,38 +15,39 @@ def main():
     # Here is what the columns define
     columns = ['servertime', 'clienttime', 'player', 'position', 'lag', 'lastknown', 'predictor', 'prediction']
     predictors = ["None", "Linear", "Weighted", "polynomial", "scaledpoly"]
+    lag = None
+    predictor = None
 
+    #dictionary stores player information
     player1 = {'time':[], 'posx':[], 'posy':[],}
     player2 = {'time':[], 'posx':[], 'posy':[],}
 
-    linecount = 0
     p1started = False
     p2started = False
 
     for line in logfile:
+        
         line = line.split(';')
         player = int(line[2])
 
-        # ignore the initial lines if they are undefined
-        if line[3]== 'undefined':
-            if not p1started or not p2started:
-                print "waiting until start"
-                continue
-        elif player == 0:
-            p1started = True
-        elif player == 1:
-            p2started = True
+        # lag should be the same for every datapoint and user
+        if int(line[4]) != lag:
+            lag = int(line[4])
+            print "Lag value is now set at: ", lag
 
-        #line = line.replace('undefined', '0,0,0')
-        
+        # predictor should be the same for every datapoint and user
+        if predictors[int(line[6])] != predictor:
+            predictor = predictors[int(line[6])]
+            print "Predictor being used is: ", predictor
+
+            
         # parse info for each player separately
         if player == 0:
+
             # if no player position info then use previous value
             if line[3] == 'undefined':
-                #player1['posx'].append(np.nan)
-                #player1['posy'].append(np.nan)
-                player1['posx'].append(player1['posx'][-1])
-                player1['posy'].append(player1['posy'][-1])
+                player1['posx'].append(np.nan)
+                player1['posy'].append(np.nan)
             else:
                 position = [int(round(float(x),0)) for x in line[3].split(",")]
                 player1['posx'].append(position[0])
@@ -55,11 +56,8 @@ def main():
 
         if player == 1:
             if line[3] == 'undefined':
-                #player2['posx'].append(np.nan)
-                #player2['posy'].append(np.nan)
-                
-                player2['posx'].append(player2['posx'][-1])
-                player2['posy'].append(player2['posy'][-1])
+                player2['posx'].append(np.nan)
+                player2['posy'].append(np.nan)
             else:
                 position = [int(round(float(x),0)) for x in line[3].split(",")]
                 player2['posx'].append(position[0])
@@ -67,12 +65,12 @@ def main():
             player2['time'].append(int(line[1]))
 
 
-    print "p1", len(player1['posx'])
-    print "p2", len(player2['posx'])
-    plotpositions(player1, player2)
-    #plotchangerate(player1, player2)
+    print "p1 data points:", len(player1['posx'])
+    print "p2 data points:", len(player2['posx'])
+    plotpositions(logname, player1, player2)
+    plotchangerate(logname, player1, player2)
 
-def plotchangerate(p1,p2):
+def plotchangerate(logname, p1,p2):
     p1time = []
     p1changerate = [] 
     for i in range(len(p1['posx'])-1):
@@ -97,22 +95,42 @@ def plotchangerate(p1,p2):
 
     P.plot(p2time, p2changerate, label='Player 2 change rate', linewidth=2)
     P.legend()
-    P.show()
+    P.title("Rate of Change")
+    P.xlabel('Time (ms)')
+    P.ylabel('Rate of Change')
+    P.savefig(logname+"_rateofchange.png")
 
+    if SHOWGRAPH:
+        P.show()
 
-#def plotdifference(p1,p2):
-    
-
-
-def plotpositions(p1, p2):
+def ploterror(logname, p1, p2):
     P.plot(p1['posx'], p1['posy'], label='Player 1', linewidth=2)
     P.plot(p2['posx'], p2['posy'], label='Player 2', linewidth=2)
     
     P.title("Player positions")
+    P.xlabel('X Coordinate')
+    P.ylabel('Y Coordinate')
+
     P.legend()
-    P.savefig("nonan.png")
-    P.show()
-    P.figure()
+    P.savefig(logname+"_positions.png")
+    
+    if SHOWGRAPH:
+        P.show()
+
+    
+# show positions during recording
+def plotpositions(logname, p1, p2):
+    P.plot(p1['posx'], p1['posy'], label='Player 1', linewidth=2)
+    P.plot(p2['posx'], p2['posy'], label='Player 2', linewidth=2)
+    
+    P.title("Player positions")
+    P.xlabel('X Coordinate')
+    P.ylabel('Y Coordinate')
+
+    P.legend()
+    P.savefig(logname+"_positions.png")
+    if SHOWGRAPH:
+        P.show()
 
 if __name__=='__main__':
     main()
